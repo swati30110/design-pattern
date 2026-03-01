5d444daf62e4de9209fa32edfb1fbfa21ee3ac65
swati30110 <swatiswagatika3010@gmail.com>
2026-03-01 19:14:19 +0530

    builder design pattern

--- creational/src/builder/User.java
+++ creational/src/builder/User.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-public class User {
+public final class User {
     private String id;
     private String username;
     private String email;
